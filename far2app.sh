de7bf12084bbd66cd720187d1e0f86e941133488
Rafuck <rfkadyrov@gmail.com>
2021-01-12 11:04:02 +0300

    Add repository content

---
+++ far2app.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+APP=Far.app
+ROOT=$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
+
+if [ "$#" -ne 1 ]; then
+    echo "App folder is not defined";
+    exit 1;
+fi
+
+if [ ! -d ${1} ]; then
+	echo "App folder is not exists";
+	exit 1;
+fi
+
+PACKAGE=$(cd ${1} && pwd)
+
+if [ ! -f $PACKAGE/bin/far2l ]; then
+	echo "${1}/bin/far2l is not exists";
+	exit 1;
+fi
+
+test $APP && rm -rf $APP
+DIR=$APP/Contents/MacOS
+mkdir -p $DIR && cd $DIR
+
+cp -r $PACKAGE/* ./
+cat >Far <<EOL
+#!/usr/bin/env bash
+ROOT="\$( cd "\$(dirname "\$0")" >/dev/null 2>&1 ; pwd -P )"
+\${ROOT}/bin/far2l & 
+EOL
+chmod a+x Far
+
+cd ../
+cp -R ${ROOT}/Contents/* ./
